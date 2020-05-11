5fa16120c18876372c482c55b6d3462fe7467e93
itok01 <contact@itok01.com>
2020-05-11 19:09:36 +0900

    件数取得APIのテストを追加

--- src/num/client.rs
+++ src/num/client.rs
@@ -11,3 +11,15 @@ pub async fn get(query: &RequestParameters) -> Result<Response, Box<dyn std::err
 
     Ok(serde_json::from_str(&text)?)
 }
+
+#[tokio::test]
+async fn get_test() {
+    let mut req_params = RequestParameters::new();
+    req_params.set_tec("1");
+
+    let resp: Response = get(&req_params).await.unwrap();
+
+    assert!(resp.giteki_information.last_update_date.is_ascii());
+    assert!(resp.giteki_information.total_count > 0);
+    assert!(resp.giteki.count > 0);
+}
